b7537f9cac7886de4fc1c6ffd837267ab9b17a4d
Baremia <rafafa.91@gmail.com>
2026-07-21 18:34:54 +0200

    Create layout.tsx

---
+++ app/app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Baremia",
+  description: "Estimaciones inteligentes para opositores.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
+}
