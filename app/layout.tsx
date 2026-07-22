08b15e376de29a9b8b34595137b544ec0f189150
Baremia <rafafa.91@gmail.com>
2026-07-22 13:50:14 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,29 @@ import { Geist } from "next/font/google";
 const geist = Geist({
   subsets: ["latin"],
 });
-
+export const metadata = {
+  title: "Baremia",
+  description:
+    "Analizamos miles de datos oficiales para estimar tu posición y ayudarte a tomar mejores decisiones.",
+  icons: {
+    icon: [
+      {
+        url: "/favicon.ico",
+      },
+      {
+        url: "/favicon-32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+      {
+        url: "/favicon-16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
+};
 export default function RootLayout({
   children,
 }: Readonly<{
