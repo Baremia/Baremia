d69ea1ca59fa85fe291f3d65680570334a7734e6
Baremia <rafafa.91@gmail.com>
2026-07-27 20:40:32 +0200

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer className="site-footer">
+      <div className="footer-inner">
+        <p>© 2026 Baremia</p>
+
+        <p>
+          Las estimaciones son orientativas y no sustituyen los resultados
+          oficiales.
+        </p>
+      </div>
+    </footer>
+  );
+}
